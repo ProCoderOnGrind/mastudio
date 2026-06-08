b989324be6a784a4ebad72e337ceac368438ad5b
Aron Taci <arontacischool@gmail.com>
2026-06-08 08:52:25 +0200

    feat: people page with group filter

---
+++ app/people/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/components/PageTitle";
+import PeopleList from "@/components/people/PeopleList";
+
+export default function PeoplePage() {
+  return (
+    <div>
+      <PageTitle>People</PageTitle>
+      <PeopleList />
+    </div>
+  );
+}
